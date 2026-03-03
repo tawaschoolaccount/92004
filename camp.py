0ec525fd6d59694ba44e92ccb57ba1eb10ac50f1
tawaschoolaccount <24121@karamu.nz>
2026-03-04 09:16:38 +1300

    Started Coding

---
+++ camp.py
@@ -0,0 +1,3 @@
+print('Tane's School Holiday Camp')
+camps_dictionary = {"Cultural Immersion":"5 Days":"Easy":"800","Kayaking And Pancakes":"3 Days":"Moderate":"400","Mountain Biking":"4 Days":"Difficult":"900"}
+print('\nThese are the activities and costs')
