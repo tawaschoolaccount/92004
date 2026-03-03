c01ed851a41a36477c04362453f8c6d02a07c757
tawaschoolaccount <24121@karamu.nz>
2026-03-04 09:41:01 +1300

    Update camp.py

--- camp.py
+++ camp.py
@@ -1,4 +1,6 @@
 print("Tane's School Holiday Camp")
+
+#List the camp options
 camps_dictionary = {
   "Cultural Immersion":["5 Days","Easy", 800],
   "Kayaking And Pancakes":["3 Days","Moderate", 400],
@@ -6,3 +8,17 @@ camps_dictionary = {
 }
 print("\nThese are the activities and costs")
 print(camps_dictionary)
+
+#Ask user for detials
+name = input("Enter your name: ")
+age = ''
+while len(age) == 0 or age.isdigit() == False:
+  age = input("Enter your age: ")
+  if len(age) == 0: 
+    print("You cannot have a blank age")
+  if len(age) != 0 and age.isdigit() == False:
+    print("Enter a valid number")
+camp_number = input("Which camp number would you like to go to? ")
+#Extra
+meal_choice = input("Would you like a standard, vegererian, or vegan meal? ")
+bus_choice = input("Do you need the shuttle bus for an additional $80? ")
