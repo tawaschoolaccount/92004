0cea74f92291ef1551bb795ca4c44d55ef87a172
tawaschoolaccount <24121@karamu.nz>
2026-03-05 10:29:03 +1300

    Added some calculation parts from the choices and added f strings to the details part

--- camp.py
+++ camp.py
@@ -30,16 +30,24 @@ while len(age) == 0 or age.isdigit() == False:
 camp_number = int(input("Which camp number would you like to go to? "))
 
 #Additional choices
-meal_choice = input("Would you like a standard, vegererian, or vegan meal? ")
-bus_choice = input("Do you need the shuttle bus for an additional $80? ")
+meal_choice = input("Would you like a standard, vegererian, or vegan meal? ").lower()
+bus_choice = input("Do you need the shuttle bus for an additional $80?(Y/N) ").upper()
+if bus_choice != "Y" and bus_choice != "N":
+  while bus_choice != "Y" and bus_choice != "N":
+    print("Please enter Y or N")
+    bus_choice = input("Do you need the shuttle bus for an additional $80?(Y/N) ").upper()
+  
+#Calculations:
+cost = camps_dictionary[camps_list[camp_number - 1]][2]
+if bus_choice == "Y": cost += 80
 
 #Final details
 print(f"\nHello {name}, you have chosen to go to the {camps_list[camp_number - 1]} (X difficulty) camp for X days. You are {age} years old. Your meal choice is {meal_choice}.")
-confirmation = input("Please confirm that you want to go to {camp_number} for the cost of $X (Y/N): ").upper()
+confirmation = input(f"Please confirm that you want to go to {camp_number} for the cost of ${cost} (Y/N): ").upper()
 if confirmation != "Y" and confirmation != "N":
   while confirmation != "Y" and confirmation != "N":
     print("Please enter Y or N")
-    confirmation = input("Please confirm that you want to go to {camp_number} for the cost of $X (Y/N): ").upper()
+    confirmation = input(f"Please confirm that you want to go to {camp_number} for the cost of ${cost} (Y/N): ").upper()
 if confirmation == "Y": print("Enjoy the camp")
 elif confirmation == "N": print("Sorry to hear that, maybe next time")
 #note: find a way to get the list of camp details from the dictionary list
