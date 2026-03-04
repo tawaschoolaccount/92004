2c524e7e973e3e33a9245160da9a71563d7f3ef3
tawaschoolaccount <24121@karamu.nz>
2026-03-05 09:40:27 +1300

    added comments

--- camp.py
+++ camp.py
@@ -6,11 +6,12 @@ camps_dictionary = {
   "Kayaking And Pancakes":["3 Days","Moderate", 400],
   "Mountain Biking":["4 Days","Difficult", 900]
 }
+
 print("\nThese are the activities and costs:")
 for item in camps_dictionary:
   print(item)
 
-#Ask user for detials
+#Ask user for details
 name = input("Enter your name: ")
 age = ''
 while len(age) == 0 or age.isdigit() == False:
@@ -20,6 +21,7 @@ while len(age) == 0 or age.isdigit() == False:
   if len(age) != 0 and age.isdigit() == False:
     print("Enter a valid number")
 camp_number = input("Which camp number would you like to go to? ")
+
 #Extra
 meal_choice = input("Would you like a standard, vegererian, or vegan meal? ")
 bus_choice = input("Do you need the shuttle bus for an additional $80? ")
